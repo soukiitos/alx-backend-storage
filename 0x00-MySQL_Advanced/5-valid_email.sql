9b642a11b9b2dded03cb27b4dce0c564a7cd4543
Soukaina Lachheb <soukaialachheb93@gmail.com>
2024-01-18 23:04:29 +0100

    Update More Tasks

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- CREATE a trigger that resets the attribute valid_email only when the email has been changed
+DROP TRIGGER IF EXISTS validate_email;
+DELIMITER $$
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+	BEGIN
+		IF OLD.emaill != NEW.email THEN
+			SET NEW.valid_email = 0;
+		ELSE
+			SET NEW.valid_email = NEW.valid_email;
+		END IF;
+	END$$
+DELIMITER;
---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,17 @@
+-- Create  a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
+BEGIN
+	DECLARE total_score INT DEFAULT 0;
+	DECLARE projects_count INT DEFAULT 0;
+
+	SELECT SUM(score) INTO total_score FROM corrections
+	WHERE corrections.user_id = user_id;
+	SELECT COUNT(*) INTO projects_count from corrections
+	WHERE corrections.user_id = user_id;
+	UPDATE users
+	SET users.averge_score = IF(projects_count = 0, 0, total_score / projects_count)
+	WHERE users.id = user_id;
+END $$
+DELIMITER ;
