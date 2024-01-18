1810b71a984462cd1f35050d120bf494da121b1a
Soukaina Lachheb <soukaialachheb93@gmail.com>
2024-01-19 00:06:37 +0100

    UPDATE OF ADVANCED TASKS

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -8,13 +8,13 @@ BEGIN
 
 	UPDATE users SET total_weighted_score = (SELECT 
 		SUM(corrections.score * projects.weight) FROM corrections
-		INNER JOIN projects ON connections.project_id = projects.id
+		INNER JOIN projects ON corrections.project_id = projects.id
 		WHERE corrections.user_id = users.id);
 	 UPDATE users SET total_weight = (SELECT
 		SUM(projects.weight) FROM corrections
-		INNER JOIN projects ON connections.project_id = projects.id
+		INNER JOIN projects ON corrections.project_id = projects.id
 		WHERE corrections.user_id = users.id);
-	UPDATE users SET users.average_score = IF(users.total_weight = 0, 0, users.total_weighted_score / total_weight);
+	UPDATE users SET users.average_score = IF(users.total_weight = 0, 0, users.total_weighted_score / users.total_weight);
 	ALTER TABLE users DROP COLUMN total_weighted_score;
 	ALTER TABLE users DROP COLUMN total_weight;
 END $$
