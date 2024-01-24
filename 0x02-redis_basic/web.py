2b3165084ce8565b12733732a5517ce9b9dffe96
Soukaina Lachheb <soukainalachheb93@gmail.com>
2024-01-25 00:38:56 +0100

    Advanced Task

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''Implementing an expiring web cache and tracker'''
+import redis
+import requests
+from datetime import timedelta
+
+
+def get_page(url: str) -> str:
+    '''Define get_page'''
+    if url is None or len(url.strip()) == 0:
+        return ''
+    redis_store = redis.Redis()
+    key = 'result:{}'.format(url)
+    request = 'count:{}'.format(url)
+    result = redis_store.get(key)
+    if result is not None:
+        redis_store.incr(request)
+        return result
+    result = requests.get(url).content.decode('utf-8')
+    redis_store.setex(key, timedelta(seconds=10), result)
+    return result
